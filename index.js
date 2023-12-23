c07bc730a61e58708f52666e77f04e114d0d1131
Sean-Harris-444 <sean.m.r.harris@gmail.com>
2023-12-23 01:10:44 -0800

    final commit bug fixes

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const rs = require("readline-sync");
 // The index2.js is the calculator that works on one line if you care to review that I would greatly appreciate it!
 
+const validOperators = ["*", "/", "+", "-"];
+
 let operator = "";
-let operatorQuestion = rs.question("What operator do you need? ");
-operator = operatorQuestion;
-if (operator === "*") {
-  console.log("thank you");
-} else if (operator === "/") {
-  console.log("thank you");
-} else if (operator === "+") {
-  console.log("thank you");
-} else if (operator === "-") {
-  console.log("thank you");
-} else {
-  console.log("invalid operator");
-  operator = rs.question("What operator do you need? ");
+
+while (true) {
+  let operatorQuestion = rs.question(
+    "What operation would you like to perform? "
+  );
+  if (validOperators.includes(operatorQuestion)) {
+    console.log(operatorQuestion + " is a valid operator");
+    operator = operatorQuestion;
+    break;
+  } else {
+    console.log(operatorQuestion + " That is not a valid operation");
+  }
 }
 
 let num1 = 0;
-let getNum1 = rs.questionInt("What is the first number? ");
+let getNum1 = rs.questionInt("What is the first number? ", {
+  limitMessage: "This is not a number",
+});
 num1 = getNum1;
 let num2 = 0;
-let getNum2 = rs.questionInt("What is the second number? ");
+let getNum2 = rs.questionInt("What is the second number? ", {
+  limitMessage: "This is not a number",
+});
 num2 = getNum2;
 
-console.log(answer(num1, operator, num2));
+console.log("The result is", answer(num1, operator, num2));
 
 function answer(x, y, z) {
   if (y === "*") {
